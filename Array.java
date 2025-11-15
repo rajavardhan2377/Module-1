a0107c49f8d1d15cc33a7f9a770262f6181acee1
rajavardhan2377 <eng24am0330@dsu.edu.in>
2025-11-16 03:12:23 +0530

    Add Array.java to read and display five numbers

---
+++ Array.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class array{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n[]=new int[5];
+        System.out.println("enter five numbers");
+        for (int i = 0; i < 5; i++) {
+            n[i]=sc.nextInt();
+            
+        }
+        System.out.println("our array has following numbers");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(n[i]);
+
+            
+        }
+    }
+}
